a218e0ea60a8eae3b407ed804d4f03212d9bb0c7
ayeolakenny <ayeolakenny@gmail.com>
2022-04-03 09:53:31 +0100

    init

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const SERVER_URI_GRAPHQL = 'https://simba-plan.herokuapp.com/graphql'
-export const CLIENT_URL = 'http://localhost:3000'
+export const CLIENT_URL = 'https://plan-client.vercel.app/'
